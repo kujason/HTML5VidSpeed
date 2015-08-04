140c9852a1af4967c67abeefeaca34f03aa4b31b
Jason <jason3ku@gmail.com>
2015-08-04 15:59:05 -0400

    Javascript bookmarklets to increase or decrease playback speed of HTML5 video

---
+++ -.js
@@ -0,0 +1,16 @@
+javascript:(function(){
+	// Decrease video speed by 0.5
+	var vid=document.getElementsByTagName("video")[0];
+	vid.playbackRate-=0.1;
+	var speedDiv = document.createElement('div');
+	speedDiv.id = "showSpeedDiv";
+	speedDiv.style.position="absolute";
+	speedDiv.style.top="0";
+	speedDiv.style.left="0";
+	speedDiv.style.color="white";
+	speedDiv.style.backgroundColor="black";
+	speedDiv.style.zIndex="2147483647";
+	vid.parentElement.appendChild(speedDiv);
+	speedDiv.innerHTML = parseFloat(Math.round(vid.playbackRate*10)/10).toFixed(1);
+	setTimeout(function(){ speedDiv.parentNode.removeChild(speedDiv) }, 3000);
+})();
